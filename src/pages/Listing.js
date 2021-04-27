2bd4b4a4b693d048cf143fbc9b2a9b3e9ae88f92
Nutod <alukodotun@gmail.com>
2021-04-27 20:34:55 +0700

    Added new component

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import { useParams } from 'react-router'
+import Layout from 'src/components/Layout'
+import { container } from 'src/styles/utils'
+import styled from 'styled-components'
+
+const ListingWrapper = styled.div`
+  ${container}
+`
 
 export default function Listing() {
+  const { id } = useParams()
+  const [data, setData] = React.useState()
+  const [loading, setLoading] = React.useState(true)
+  const [error, setError] = React.useState(null)
+
+  React.useEffect(() => {
+    fetch('http://fakeapi.jsonparseronline.com/posts/1')
+      .then(response => response.json())
+      .then(json => {
+        setLoading(false)
+        setData(json)
+      })
+      .catch(error => {
+         setLoading(false)
+         setError(error)
+      })
+  }, [id])
+
   return (
-    <div>
-      <h2>Listing with an id</h2>
-    </div>
+    <Layout>
+      <ListingWrapper>
+        <h2>Listing with an id</h2>
+      </ListingWrapper>
+    </Layout>
   )
 }
