20dbaea7b1765fe18927378ab0213140d7159bbc
Nutod <alukodotun@gmail.com>
2021-04-29 21:50:53 +0700

    Updated use Auth

--- src/context/useAuth.js
+++ src/context/useAuth.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import constate from 'constate'
 
+// Use this in production
+export async function bootstrapAppData() {
+  let user = null
+
+  return user
+}
+
+export function userGenerator(delay = 1000) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve({ username: 'username' }), delay)
+  })
+}
+
 function useAuth() {
   const [user, setUser] = React.useState(null)
+
+  // React.useEffect(() => {
+  //   ;(async () => {
+  //     const generatedUser = await userGenerator()
+
+  //     // console.log(generatedUser)
+  //     setUser(generatedUser)
+  //   })()
+  // }, [])
 
   return { user, setUser }
 }
